61268ca3c314c1d6fcd66faf3f41f97a9355c441
abdulahadkhan-10 <st.abdul24@gmail.com>
2025-06-25 18:23:56 +0530

    feat: Protect shop and bucket routes with authentication middleware

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -1,10 +1,20 @@
-// routes/shopRoutes.js
+//shop.route.js
 import express from 'express';
-import { createShop } from '../controller/shop.controller.js';
+import {
+  createShop,
+  addItemsToBucket,
+  removeItemsFromBucket,
+} from '../controller/shop.controller.js';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
-// Create a shop (only for authenticated users)
-router.post('/createShop', createShop);
+// SHOP ROUTES
+router.post('/createShop', protect, createShop);
+
+// BUCKET ROUTES
+router.post('/bucket/add', protect, addItemsToBucket);
+router.delete('/bucket/remove', protect, removeItemsFromBucket);
+
 
 export default router;
